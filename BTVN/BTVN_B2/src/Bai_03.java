db42573d18c0b33935b402dabaca5a2aff1f5d1c
Chí Tình <chitinhhaui@gmail.com>
2020-10-04 23:58:28 +0700

    BTVN Updating...

--- BTVN/BTVN_B2/src/Bai_03.java
+++ BTVN/BTVN_B2/src/Bai_03.java
@@ -61,12 +61,14 @@ public class Bai_03 {
 
         } while (true);
     }
-    public static void show_array(int []a, int n) {
+
+    public static void show_array(int[] a, int n) {
         for (int i = 0; i < n; i++) {
             System.out.printf(a[i] + "  ");
         }
         System.out.println();
     }
+
     public static void insert_element(int[] a, int n) {
         System.out.printf("Enter the location to insert: ");
         int index = scanner.nextInt();
@@ -74,18 +76,19 @@ public class Bai_03 {
         int x = scanner.nextInt();
         if (index >= 0 && index <= n) {
             n++;
-            for (int i = n; i > index ; i--)
+            for (int i = n; i > index; i--)
                 a[i] = a[i - 1];
-            a[index ] = x;
+            a[index] = x;
         }
         System.out.printf("Your array after insert: ");
         show_array(a, n);
     }
-    public static void delete_element(int []a, int n){
+
+    public static void delete_element(int[] a, int n) {
         System.out.printf("Enter the location to delete: ");
         int index = scanner.nextInt();
         if (index >= 0 && index <= n) {
-            for (int i = index; i<n ; i++) {
+            for (int i = index; i < n; i++) {
                 a[i] = a[i + 1];
                 n--;
             }
@@ -93,7 +96,8 @@ public class Bai_03 {
         System.out.printf("Your array after insert: ");
         show_array(a, n);
     }
-    public static void reverse_array(int []a, int n) {
+
+    public static void reverse_array(int[] a, int n) {
         System.out.printf("Your array after reverse: ");
         for (int i = n - 1; i >= 0; i--) {
             System.out.printf(a[i] + "  ");
@@ -101,12 +105,11 @@ public class Bai_03 {
         System.out.println();
     }
 
-    public static void a1_and_request(int []a, int n) {
+    public static void a1_and_request(int[] a, int n) {
         System.out.println("A[1] = " + a[1]);
         System.out.printf("Number divisible A[1]: ");
         for (int i = 0; i < n; i++) {
             if (a[i] % a[1] == 0) {
-//                System.out.printf(String.valueOf(a[i]) + "  ");
                 System.out.printf(a[i] + "  ");
             }
         }

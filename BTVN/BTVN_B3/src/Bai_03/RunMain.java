e1b594b3b4834bebc4ebae2c6e35cfb4f8e0221d
Chí Tình <chitinhhaui@gmail.com>
2020-10-11 21:58:30 +0700

    BTVN Lesson 03

--- BTVN/BTVN_B3/src/Bai_03/RunMain.java
+++ BTVN/BTVN_B3/src/Bai_03/RunMain.java
@@ -5,17 +5,13 @@ public class RunMain {
         HangHoa FirstItem = new HangHoa("CMTT_1945", "Bột Giặt Omo", 90000, 3);
         HangHoa SecondItem = new HangHoa();
         SecondItem.setMaHang("GPMN_1975");
-        SecondItem.setTenHang("Dầu Ăn Simply");
+        SecondItem.setTenHang("Dầu Ăn Simply (Chai 1 lít)");
         SecondItem.setDonGia(45000);
         SecondItem.setSoLuong(5);
         System.out.println("----------------------------------------------");
-//        System.out.println("-----First Item Information-----");
         FirstItem.Xuat();
         System.out.println("----------------------------------------------");
-//        System.out.println("-----Second Item Information-----");
         SecondItem.Xuat();
         System.out.println("----------------------------------------------");
-
     }
-
 }

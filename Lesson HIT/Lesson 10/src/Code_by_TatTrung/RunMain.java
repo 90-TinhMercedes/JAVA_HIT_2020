fa2b1e9455a9adf8e18d5e9a8e47d758f222efb8
Chí Tình <chitinhhaui@gmail.com>
2020-11-28 22:30:00 +0700

    Java HIT

---
+++ Lesson HIT/Lesson 10/src/Code_by_TatTrung/RunMain.java
@@ -0,0 +1,19 @@
+package demo;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class RunMain {
+    public static void main(String[] args) {
+        SQLController sqlController = new SQLController();
+        try{
+            ResultSet rs = sqlController.SelectAllByTable("HangHoa");
+            while (rs.next()){
+                System.out.println(rs.getInt(1) + ":" + rs.getString(2));
+            }
+            sqlController.CloseConnection();
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+}
